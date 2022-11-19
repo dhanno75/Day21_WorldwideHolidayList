94eaa314d9076b6dff61fb84833d26b1abc06f26
dhanno75 <pdhananjay774@gmail.com>
2022-11-19 13:40:47 +0530

    worldwide holiday task success

---
+++ script.js
@@ -0,0 +1,37 @@
+let url =
+  "https://calendarific.com/api/v2/countries?api_key=248b0a97898564f4c3c2672cb6dfe64f407f3bfd";
+
+const getCountryDetails = async () => {
+  try {
+    const cats = await fetch(url, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await cats.json();
+    // console.log(data.response.countries);
+    for (let country of data.response.countries) {
+      renderHolidayDetails(country);
+    }
+  } catch (err) {
+    document.querySelector(".main-container").innerHTML =
+      "Something went wrong, Please try after some time!";
+  }
+};
+getCountryDetails();
+
+const renderHolidayDetails = (country) => {
+  document.querySelector(".main-container").innerHTML += `
+    <div class='container'>
+      <h2><span> ${country.country_name}</span></h2>
+      <ul>
+        <li>Total Holidays: ${country.total_holidays}</li>
+        <li>Supported Languages: ${country["supported_languages"]}</li>
+        <li>Country code: ${country["iso-3166"]}</li>
+        <li>UUID: ${country.uuid}</li>
+      </ul>
+    </div>
+  `;
+};
